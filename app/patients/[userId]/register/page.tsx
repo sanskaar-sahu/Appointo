9ff0d4bdd04f84c903683b4ba1a92aee18f9585d
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-10-06 02:04:20 +0530

    Deployment fix

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,7 +3,6 @@ import { redirect } from "next/navigation";
 
 import RegisterForm from "@/components/forms/RegisterForm";
 import { getPatient, getUser } from "@/lib/actions/patient.actions";
-import * as Sentry from '@sentry/nextjs'
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
